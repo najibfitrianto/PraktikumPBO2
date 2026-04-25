db2fdc55281c9862d473760524a6f41bf9bbfcd7
najibfitrianto <najibfitrianto@gmail.com>
2026-04-25 12:20:09 +0700

    Update file Praktikum 2

--- src/Titik.java
+++ src/Titik.java
@@ -1,5 +1,86 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+/* Nama File    : Titik.java
+ * Deskripsi     : Berisi atribut dan method dalam class Titik
+ * Pembuat       : Mohammad Najib Fitrianto
+ */
+
+public class Titik {
+    private double absis;
+    private double ordinat;
+    private static int counterTitik = 0;
+
+    public Titik() {
+        this(0, 0);
+    }
+
+    public Titik(double absis, double ordinat) {
+        this.absis = absis;
+        this.ordinat = ordinat;
+        counterTitik++;
+    }
+
+    public double getAbsis() {
+        return absis;
+    }
+
+    public double getOrdinat() {
+        return ordinat;
+    }
+
+    public static int getCounterTitik() {
+        return counterTitik;
+    }
+
+    public void setAbsis(double x) {
+        absis = x;
+    }
+
+    public void setOrdinat(double y) {
+        ordinat = y;
+    }
+
+    public void geser(double x, double y) {
+        absis = absis + x;
+        ordinat = ordinat + y;
+    }
+
+    public int getKuadran() {
+        if (absis > 0 && ordinat > 0) {
+            return 1;
+        } else if (absis < 0 && ordinat > 0) {
+            return 2;
+        } else if (absis < 0 && ordinat < 0) {
+            return 3;
+        } else if (absis > 0 && ordinat < 0) {
+            return 4;
+        }
+        return 0;
+    }
+
+    public double getJarakPusat() {
+        return Math.sqrt(Math.pow(absis, 2) + Math.pow(ordinat, 2));
+    }
+
+    public double getJarak(Titik T) {
+        return Math.sqrt(Math.pow(this.absis - T.getAbsis(), 2) + Math.pow(this.ordinat - T.getOrdinat(), 2));
+    }
+
+    public void refleksiX() {
+        ordinat = -ordinat;
+    }
+
+    public void refleksiY() {
+        absis = -absis;
+    }
+
+    public Titik getRefleksiX() {
+        return new Titik(this.absis, -this.ordinat);
+    }
+
+    public Titik getRefleksiY() {
+        return new Titik(-this.absis, this.ordinat);
+    }
+
+    public void printTitik() {
+        System.out.println("Titik (" + absis + "," + ordinat + ")");
     }
 }
